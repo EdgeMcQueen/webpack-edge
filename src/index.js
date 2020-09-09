3e75e3cd6de8f637490e8fa538fd428f732891d1
sardormutalov <sardor.mutalov@gmail.com>
2020-09-09 10:11:55 +0300

    css, scss loaders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './js/common'
+import './css/main.css'
+import './scss/main.scss'
 
 // import 'vue'
 // import Vue from 'vue'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	entry: {
@@ -16,9 +17,40 @@ module.exports = {
 				loader: "babel-loader",
 				exclude: "/node_modules/",
 			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader",
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
+				],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader"
+				],
+			},
 		],
 	},
 	devServer: {
 		overlay: true,
 	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+		}),
+	],
 };
